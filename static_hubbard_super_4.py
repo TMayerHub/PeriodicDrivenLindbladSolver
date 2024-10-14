e60f82539f794dd90c33ac5e87feb55267750d33
Theresa Mayer <theresa.mayer@student.tugraz.at>
2024-10-14 08:35:45 +0200

    implimemented time evolution with quspin

--- static_hubbard_super_4.py
+++ static_hubbard_super_4.py
@@ -11,9 +11,9 @@ from quspin.basis import spinful_fermion_basis_1d
 import numpy as np # generic math functions
 import scipy
 import matplotlib.pyplot as plt
-
+# %%
 ##### define model parameters #####
-L=4# system size
+L=5# system size
 center=int(np.floor(L/2))
 T=np.sqrt(2.0)*np.ones(L)+0j#hopping right
 U=1
@@ -26,7 +26,7 @@ Gamma2=np.ones(L)*0.7
 #total lenght is N=4L, due to spin up and down
 basis = spinful_fermion_basis_1d(2*L)
 
-
+# %%
 def defineH_super(L,T,U,eps,basis):
     if np.isscalar(T):
         T=np.ones(L)*T
@@ -80,9 +80,6 @@ def define_Dissipators1(L,Gamma1,basis):
     part2 = [[-1*Gamma1[i],i,i] for i in range(L)]
     part3 = [[-1*Gamma1[i],L+i,L+i] for i in range(L)]
     
-    #part1 = [[-2j*Gamma1[i]*(1j)*(-1)**((2**(4*L)-i-2).bit_count()),i,(L+i)] for i in range(L)]
-    #part2 = [[-1*Gamma1[i],i,i] for i in range(L)]
-    #part3 = [[-1*Gamma1[i],L+i,L+i] for i in range(L)]
     
     static=[
         #spin up
@@ -104,14 +101,6 @@ def define_Dissipators2(L,Gamma2,basis):
     part2 = [[-1*Gamma2[i],i,i] for i in range(L)]
     part3 = [[-1*Gamma2[i],L+i,L+i] for i in range(L)]
     
-    #add phase due to augmented space
-    #part1 = [[-2j*Gamma2[i]*(1j)*(-1)**((2**(4*L)-i).bit_count()),i,(L+i)] for i in range(L)]
-    #part2 = [[-1*Gamma2[i],i,i] for i in range(L)]
-    #part3 = [[-1*Gamma2[i],L+i,L+i] for i in range(L)]
-    #part4 = [[-2j*Gamma2[i],] for i in range(L)]
-    #print(part1)
-    #print(part2)
-    #print(part3)
     static=[
         #spin up
         ["++|",part1],
@@ -137,7 +126,8 @@ def get_Lindbladoperator(L,T,U,eps,Gamma1,Gamma2,basis=0):
     D1=define_Dissipators1(L,Gamma1,basis)
     D2=define_Dissipators2(L,Gamma2,basis)
 
-    return -1j*H_super+D1+D2
+    #return -1j*H_super+D1+D2
+    return H_super+1j*(D1+D2)  # multiply by -i due to .evolve structure
 
 def get_leftVacuum(L_static):
     basis=L_static.basis
@@ -168,23 +158,13 @@ def get_leftVacuum(L_static):
 
                 #count number of occupied states in Fockspace
                 n=int(spin_up_fock).bit_count()+int(spin_down_fock).bit_count()
-                #print(format(spin_up_fock,'0{}b'.format(L)),\
-                #              format(spin_down_fock,'0{}b'.format(L)))
-                #print(n)
-                #calculate phase
-                #if n%2:
-                    #data.append((-1j)**(n))
-                #else:
-                    #data.append((-1j)**(n)*(-1))
+
                 if n%2:
                     data.append(1j*(-1)**L)
                 else: data.append((-1)**(L-int(spin_up_fock).bit_count()))
-                #data.append((-1j)**(n))
-                #data.append(1)
-                #data.append((1)**n)
+
                 row_ind.append(k)
-                #print(format(spin_up,'0{}b'.format(2*L)),format(spin_down,'0{}b'.format(2*L)))
-                #print(k,(-1j)**(n**2))
+
     col_ind=np.zeros(len(data),dtype=int)
     data=np.array(data)
     
@@ -196,10 +176,15 @@ def get_leftVacuum(L_static):
         
 
 def differential_rho(t,rho_n):
-    return L_static_csr@rho_n
-
-def differential_rho_T(t,rho_n_T):
-    return rho_n_T@L_static_csr.T
+    #return L_static_csr@rho_n
+    return L_static.dot(rho_n)
+    
+def differential_rho_T(t,rho_nT):
+    rho_n=np.array([rho_nT]).T
+    print('rho_n',rho_n)
+    return L_static.rdot(rho_n)
+    #return rho_n_T@L_static_csr.T
+    
 
 def n(i,rho,basis,leftVacuum):
     #i counts from the middle
@@ -223,11 +208,10 @@ def n(i,rho,basis,leftVacuum):
 
     return n_exp[0]
 
-def exact_Diagonalization(L_static_csr):
+def exact_Diagonalization(L_static):
+    L_static_csr=L_static.as_sparse_format()
     w,vl,vr=scipy.linalg.eig(L_static_csr.toarray(),left=True,right=True)
-    
     w_min=np.argmin(abs(w))
-    #print(w_min)
 
     rho_inf=np.array([vr[:,w_min]]).T
 
@@ -239,108 +223,98 @@ def exact_Diagonalization(L_static_csr):
     
     return vl0,rho_inf
 
-def Diag_rho_inf(L_static_csr):
-    w,rho_inf=scipy.sparse.linalg.eigsh(L_static_csr,k=1,sigma=0)
+def lowestEV(L_static):
+    w,rho_inf=L_static.eigsh(k=1,sigma=0)
+    print('w rho inf: ', w)
     norm=leftVacuum.H@rho_inf
     return rho_inf/norm
         
+def compareVectors(v1,v2):
+    print('difference',np.sum(abs(v1-v2)))
+    print('direct comparison')
+    correct=[]
+    false=[]
+    
+    for i_1, data_1 in zip(v1.indices,v1.data):
+        data_2=leftVacuum[i_1].data
+        #if leftVacuum[i_vl].data != 
+        if len(data_2)==0:
+            data_2=0
+        else:
+            data_2=data_2[0]
+        num=int(2**(4*L)-i_1-1).bit_count()/2
+        if abs(data_1-data_2)>1e-10:
+            #print(format(2**(4*L)-i_vl-1,'0{}b'.format(4*L)), num)
+            #print(i_vl,'    ',f"{data_vl:.3}", data_V)
+            false.append(num)
+        else:
+            if abs(data_1)>1e-10:
+                correct.append(num)
+        if abs(data_1)>1e-10:
+            print(format(2**(4*L)-i_1-1,'0{}b'.format(4*L)), num)
+            print(f"{data_1:.3}")
+            print('')
+    
+    print(false)     
+    print(correct)      
+    print()  
         
-        
-        
-        
-#print(basis)
 
+
+# %%
 #calculate the Lindbladoperator
 L_static=get_Lindbladoperator(L,T,U,eps,Gamma1,Gamma2,basis)
 print('define left vacuum')
 leftVacuum=get_leftVacuum(L_static)
-print('operator to csr')
-#L_static_csr=L_static.tocsr()
 
-
-
-
-#calculate leftVacuum
-leftVacuum=get_leftVacuum(L_static)
-
-##test leftVacuum
-vl0,rho_inf=exact_Diagonalization(L_static_csr)
-#finding rho_inf
-print('computing rho_inf')
-rho_inf=Diag_rho_inf(L_static_csr)
-# %%
-
-
-vl0[abs(vl0)< 1e-15]=np.real(vl0[abs(vl0)< 1e-15])*0
-
-
-#print(np.sum(abs(leftVacuum.H@L_static_csr)))
-
-
-#print('vl0')
-#print(vl0)
-#print('leftVacuum')
-#print(leftVacuum)
+print('calculate EV')
+#rho_inf=lowestEV(L_static)
 
 # %%
-
-#print('basis index:  ',basis.index(format(3,'0{}b'.format(2*L)),\
-#              format(3,'0{}b'.format(2*L))))
-print()
-print('difference',np.sum(abs(vl0-leftVacuum)))
-print('direct comparison')
-correct=[]
-false=[]
-
-for i_vl, data_vl in zip(vl0.indices,vl0.data):
-    data_V=leftVacuum[i_vl].data
-    #if leftVacuum[i_vl].data != 
-    if len(data_V)==0:
-        data_V=0
-    else:
-        data_V=data_V[0]
-    num=int(2**(4*L)-i_vl-1).bit_count()/2
-    if abs(data_vl-data_V)>1e-10:
-        #print(format(2**(4*L)-i_vl-1,'0{}b'.format(4*L)), num)
-        #print(i_vl,'    ',f"{data_vl:.3}", data_V)
-        false.append(num)
-    else:
-        if abs(data_vl)>1e-10:
-            correct.append(num)
-    if abs(data_vl)>1e-10:
-        print(format(2**(4*L)-i_vl-1,'0{}b'.format(4*L)), num)
-        print(f"{data_vl:.3}")
-        print('')
-
-print(false)     
-print(correct)      
-print()
-# %%
-    
-
-
 
 #initial rho
-rho0=leftVacuum.toarray()
+rho0=leftVacuum#.toarray()
 
 
 #calculate timeevolution of rho
 t0=0
 tf=1e2
-dt=0.01
+dt=0.1
 
 t=np.linspace(t0,tf,int(tf/dt)+1)
+#print('calculate rdot: ',L_static.rdot(rho0))
 
-#rho_0T=rho0.T
+
+rho0T=rho0.T.toarray()
 #rho=scipy.integrate.solve_ivp(differential_rho_T,(t0,tf),rho_0T[0]+0j,method='RK45',t_eval=t)
 #rho=rho['y']
+#print('type: ',type(rho0T))
+#print(rho0T[0])
+#print('calculate rdot: ',L_static.rdot(rho0T[0]))
+#print('calculate norm')
+#rho_dot=L_static.dot(rho0)
+#leftVacuumH=leftVacuum.H
+#norm=leftVacuumH@rho_dot
+#print('norm')
+#print(norm.toarray())
 
-#n_exp=n(0,rho,basis,leftVacuum)
+#rho=scipy.integrate.solve_ivp(differential_rho_T,(t0,tf),rho0T[0]+0j,method='RK45',t_eval=t)
+#rho=rho['y']
+
+#check that inital is not multiplied by -i
+rho=L_static.evolve(rho0T[0],0,t)
+rho_end=rho[:,-1]
+#print(rho_end)
+#print(np.shape(rho_end))
+#print(rho_inf)
+print('n_exp')
+n_exp=n(0,rho_end,basis,leftVacuum)
 #print('n_time evolved: ',n_exp[-1])
 
 
-n_inf=n(0,rho_inf,basis,leftVacuum)
-print('n_inf',n_inf)
+#n_inf=n(0,rho_inf,basis,leftVacuum)
+print('n_exp',n_exp)
+#print('n_inf',n_inf)
 print('n_analytic',2*Gamma2[0]/(Gamma1[0]+Gamma2[0]))
 
 
