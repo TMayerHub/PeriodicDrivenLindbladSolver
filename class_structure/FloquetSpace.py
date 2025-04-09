added the calculation of the current using the Meir-Wingreen formula(analytic and numeric), fixed indices of full gamma

class_structure/FloquetSpace.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 import multiprocessing
 import os
 
-print(multiprocessing.cpu_count())
-print(os.cpu_count())
+#print(multiprocessing.cpu_count())
+#print(os.cpu_count())
 #print(len(os.sched_getaffinity(0)))
 
 def loadTimeJson(filePath):
@@ -131,7 +131,7 @@ def RetartedWigner(_input,mode_max,site,t,Tau_total,adag_a,a_adag):
     #plt.figure()
     #plt.title('test retarded')
     for m in range(-mode_max,mode_max+1):
-        print('modes in retarded:',m,m+mode_max)
+        #print('modes in retarded:',m,m+mode_max)
         a_adagWigner0=np.concatenate((np.zeros(len(Tau)-1),calculateWigner_t(_input,a_adag[0],m,Tau,t)))
         adag_a_conjWigner0=np.concatenate((np.zeros(len(Tau)-1),calculateWigner_t(_input,np.conj(adag_a[0]),m,Tau,t)))
 
@@ -435,9 +435,12 @@ def calculateWigner(_output,mode_max,components,sites):
                
 def calcGreen(_input,component):
     eps=_input['parameters']['epsilon']
-    gamma1=np.diag(_input['parameters']['coupling_empty'],0)
-    gamma2=np.diag(_input['parameters']['coupling_full'],0)
-    hopping=_input['parameters']['hopping']*np.ones(len(eps)-1)
+    #gamma1=np.diag(_input['parameters']['coupling_empty'],0)
+    #gamma2=np.diag(_input['parameters']['coupling_full'],0)
+    gamma1=np.array(_input['parameters']['coupling_emptyReal'])+1j*np.array(_input['parameters']['coupling_emptyImag'])
+    gamma2=np.array(_input['parameters']['coupling_fullReal'])+1j*np.array(_input['parameters']['coupling_fullImag'])
+    #hopping=_input['parameters']['hopping']*np.ones(len(eps)-1)
+    hopping=_input['parameters']['hopping'][:-1]
     hopping_left=np.conj(hopping)
     
     E=np.diag(eps,0)+np.diag(hopping,1)+np.diag(hopping_left,-1)
@@ -445,14 +448,14 @@ def calcGreen(_input,component):
     Gamma_minus=gamma2-gamma1
     i0=component[0]+int(np.floor(len(eps)/2))
     i1=component[1]+int(np.floor(len(eps)/2))
-    omegas=np.linspace(-5,5,1000)
+    omegas=np.linspace(-10,10,1000)
     Gr=np.zeros(len(omegas))*1j
     Gk=np.zeros(len(omegas))*1j
     for i in range(len(omegas)):
         omega=np.diag(omegas[i]*np.ones(len(eps)))
         Gr_matrix=np.linalg.inv(omega-E+1j*Gamma_plus)
         Gr[i]=Gr_matrix[i0,i1]
-        Gk_matrix=2j*Gr_matrix@Gamma_minus@(np.conj(Gr_matrix))
+        Gk_matrix=2j*Gr_matrix@Gamma_minus@(np.conj(Gr_matrix).T)
         Gk[i]=Gk_matrix[i0,i1]
     return omegas,Gr,Gk
 
@@ -488,8 +491,8 @@ def calculateWignerFromFile(file,mode_max,components,sites):
             a_adag=[a_adag0,a_adag1]
             
             for comp in components:
-                if comp=='retarted':
-                    wigner_dic[site['sites']]['retarted']=RetartedWigner(_input,mode_max,site,t,Tau_total,adag_a,a_adag)
+                if comp=='retarded':
+                    wigner_dic[site['sites']]['retarded']=RetartedWigner(_input,mode_max,site,t,Tau_total,adag_a,a_adag)
                 if comp=='lesser':
                     wigner_dic[site['sites']]['lesser']=LesserWigner(_input,mode_max,site,t,Tau_total,adag_a)
                 if comp=='greater':
@@ -599,10 +602,36 @@ def calculateCurrentFromKeldysh(file,sites):
         print('current: ',current)
         return sites, current
 
-file='class_structure/results/U1V1Om1_20250325-212458.json'
+# file='class_structure/results/U0V0Om0_20250408-134137.json'
+# _input,_output=loadTimeJson(file)
 
-calculateCurrentFromFile(file,['0 1'])
-calculateCurrentFromKeldysh(file,['0 1'])
+# sites,omegas_wigner,wigner_dic=calculateWignerFromFile(file,0,['retarded','keldysh','greater','lesser'],['0 0'])
+# omegas,Gr,Gk=calcGreen(_input,[0,0])
+# print(_input)
+# print(len(omegas_wigner))
+# om_start=10
+# mask=(omegas_wigner > -om_start) & (omegas_wigner < om_start)
+# plt.figure()
+# plt.title('retarded')
+# plt.plot(omegas,Gr.imag)
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['retarded'][0][mask].imag,linestyle='dashed')
+# plt.plot(omegas,Gr.real)
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['retarded'][0][mask].real,linestyle='dashed')
+# #plt.show(block=False)
+# plt.show(block=False)
+
+# plt.figure()
+# plt.title('keldysh')
+# plt.plot(omegas,Gk.imag)
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['greater'][0][mask].imag)
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['lesser'][0][mask].imag)
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['keldysh'][0][mask].imag,linestyle='dashed')
+# plt.plot(omegas_wigner[mask],wigner_dic['0 0']['greater'][0][mask].real)
+# plt.show()
+
+#calculateCurrentFromFile(file,['0 1'])
+#calculateCurrentFromKeldysh(file,['0 1'])
+
 #calculateFloquetFromFile(file,[2,2],['greater'],['0 0'])
 
 #_input,_output=loadTimeJson('results/U0V1Om1_20250212-092450.json')

class_structure/GreensFunction_sites.py
@@ -6,7 +6,11 @@ Created on Mon Dec  2 08:02:54 2024
 @author: theresa
 """
 
+import sys
 import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+#if current_dir not in sys.path:
+#    sys.path.append(current_dir)
 os.environ["KMP_DUPLICATE_LIB_OK"] = ("True") 
 os.environ["OMP_NUM_THREADS"] = str(4)# set number of OpenMP threads to run in parallel
 os.environ["MKL_NUM_THREADS"] = str(4)# set number of MKL threads to run in parallel
@@ -15,8 +19,8 @@ import json
 #os.environ["MKL_NUM_THREADS"] = str(4)
 #os.environ['OMP_NUM_THREADS'] = '4'
 
-from Lindblatt import createLindblad
-from augmented_basis import augmented_basis
+from class_structure.Lindblatt import createLindblad
+from class_structure.augmented_basis import augmented_basis
 
 from quspin.operators import hamiltonian
 
@@ -66,10 +70,10 @@ class calculateGreensFunction:
         self.basisM=self._basisM()
         self.basisP=self._basisP()
         print('end define basis')
-        print(self.basis0.Ns)
-        print(self.basisE.Ns)
-        print(self.basisM.Ns)
-        print(self.basisP.Ns)
+        #print(self.basis0.Ns)
+        #print(self.basisE.Ns)
+        #print(self.basisM.Ns)
+        #print(self.basisP.Ns)
         
         self.leftVacuum=self._leftVacuum()
         #self.plus_lV=self.plus_leftVacuum()
@@ -704,7 +708,7 @@ class calculateGreensFunction:
 
     def _GreaterLesserSites(self,sites,dt=0.05,eps=1e-12,max_iter=1000,
                         av_periods=5,tf=5e2,t_step=1e2,av_Tau=10,writeFile=False,
-                        dirName=None):
+                        dirName=None,file_name=None,fit_parameters=None):
         
         res_sites=[]
         t_res=[]
@@ -736,11 +740,21 @@ class calculateGreensFunction:
                 res_sites[i]['a_a+ imag'].append(zeros)
                 
         
-               
+        parameters_json = {key: value.tolist() if isinstance(value, np.ndarray) else value
+                   for key, value in self.parameters.items()}
+        
+        parameters_json['coupling_emptyImag']=np.imag(parameters_json['coupling_empty']).tolist()
+        parameters_json['coupling_emptyReal']=np.real(parameters_json['coupling_empty']).tolist()
+        parameters_json['coupling_fullImag']=np.imag(parameters_json['coupling_full']).tolist()
+        parameters_json['coupling_fullReal']=np.real(parameters_json['coupling_full']).tolist()
+
+        del parameters_json['coupling_empty']
+        del parameters_json['coupling_full']
+
         if writeFile:
             timestr = time.strftime("%Y%m%d-%H%M%S")
             _input = {
-                'parameters':self.parameters,
+                'parameters':parameters_json,
                 'spin':self.spin,
                 'sites':sites,
                 'time_param': {'dt':dt,
@@ -753,7 +767,8 @@ class calculateGreensFunction:
 
                                }
                 }
-            
+            if not(fit_parameters == None):
+                _input['fit_parameters'] = fit_parameters
             _output={
                        't':t_res,
                        'Tau':Tau_res,
@@ -764,11 +779,16 @@ class calculateGreensFunction:
                        'input':_input,
                        'output':_output,
                        }
+            if dirName:
+                os.makedirs(dirName, exist_ok=True)
+            if file_name==None:
+                filename = ('U'+str(self.parameters['interaction']) +
+                'V'+str(self.parameters['drive'])+
+                'Om'+str(self.parameters['frequency'])+
+                '_'+timestr+'.json')
             
-            filename = ('U'+str(self.parameters['interaction']) +
-            'V'+str(self.parameters['drive'])+
-            'Om'+str(self.parameters['frequency'])+
-             '_'+timestr+'.json')
+            else:
+                filename=file_name
             
             filepath=os.path.join(dirName, filename )
             
@@ -808,7 +828,7 @@ class calculateGreensFunction:
                 diff_period=1
                 break
             t_period_diff=t[-(1+j)*N_period-1:-j*N_period]
-            print(len(t_period_diff))
+            #print(len(t_period_diff))
             diff_period+=np.sum(np.trapz(abs(rhos[:,-N_period-1:]-rhos[:,-(j+1)*N_period-1:-j*N_period]),t_period_diff,axis=1))/period/len(rhos)/av_periods
         num_periods_step=int(np.ceil(t_step/period))
 
@@ -822,8 +842,8 @@ class calculateGreensFunction:
             
             t_total=np.append(t_total,t)
             rhos_total=np.append(rhos_total,rhos,axis=1)
-            print(t_total.shape)
-            print(rhos_total.shape)
+            #print(t_total.shape)
+            #print(rhos_total.shape)
                 
             if i>max_iter:
                 if return_all:

class_structure/Lindblatt.py
@@ -94,7 +94,7 @@ class createLindblad:
         
         if np.isscalar(self.Gamma1):
             if self.L == 1:
-                self.Gamma1 = [self.Gamma1]
+                self.Gamma1 = np.array([[self.Gamma1]])
             
             else:
                 self.Gamma1 = np.ones(self.L)*self.Gamma1
@@ -108,7 +108,7 @@ class createLindblad:
         
         if np.isscalar(self.Gamma2):
             if self.L == 1:
-                self.Gamma2 = [self.Gamma2]
+                self.Gamma2 = np.array([[self.Gamma2]])
             
             else:
                 self.Gamma2 = np.ones(self.L)*self.Gamma2
@@ -119,20 +119,33 @@ class createLindblad:
                               same coupling on every site (except for the central site
                                where gamma1 = 0)"""
                               ,category=UserWarning)
-        
+        #print('gamma shape',len(np.shape(self.Gamma1)))
+        if len(np.shape(self.Gamma1))==1:
+            self.Gamma1=np.diag(self.Gamma1)
+        #print(self.Gamma1)
+        if len(np.shape(self.Gamma2))==1:
+            self.Gamma2=np.diag(self.Gamma2)
+
         sym = True
         for parameter_name,name in zip(['eps', 'T', 'Gamma1', 'Gamma2'],
                                        ['epsilon', 'hopping', 'coupling_empty', 'coupling_full']):
             parameter = getattr(self, parameter_name)
+            #print(name)
             if not(len(parameter)==self.L or len(parameter)==2*self.L):
                    raise ValueError(f''''the lenght of the parameterlist for
                                     {name} has invalid length it should be a scalar
                                     L or 2L long''')
                                     
             if len(parameter)==self.L:
-                parameter=np.concatenate((parameter,parameter))
+                if parameter_name=='Gamma1' or parameter_name=='Gamma2':
+                    N=parameter.shape[0]
+                    parameter_new=np.zeros((2*N, 2*N), dtype=parameter.dtype)
+                    parameter_new[:N, :N] = parameter
+                    parameter_new[N:2*N, N:2*N] = parameter
+                else:
+                    parameter_new=np.concatenate((parameter,parameter))
                 
-                setattr(self, parameter_name, parameter)
+                setattr(self, parameter_name, parameter_new)
             
             if len(parameter)==2*self.L and not((parameter[:self.L]==parameter[self.L:]).all()):
                 sym = False
@@ -168,6 +181,7 @@ class createLindblad:
     
     def defineH_super(self):
         L=self.L
+        self.T=self.T[:-1]
         T_c=np.conjugate(self.T)
        #Hamiltonian acting on Fockspace (Fockspace are the odd sites)  
         hop_left_up = [[self.T[int((i-1)/2)],i,(i+2)] for i in range(1,2*L-2,2)] 
@@ -232,30 +246,49 @@ class createLindblad:
     #removes particels from the system
     def define_Dissipators1(self):
         L=self.L
+        
         #mixed Fock and augmented
-        part1 = [[-2j*self.Gamma1[int(i/2)],(i+1),i] for i in range(0,4*L,2)]
+        part1 = []
+        #print(self.Gamma1)
+        for i in range(0, 4*L, 2):
+            for j in range(1, 4*L, 2):
+                gamma_value = self.Gamma1[int(i/2), int((j-1)/2)]
+                #print(gamma_value, i, j)  # This will print the values
+                part1.append([-2j * gamma_value, j, i])  # Adding to part1 list
+        #part1 = [[-2j*self.Gamma1[int(i/2),int((j-1)/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
         #Fock
-        part2 = [[-1*self.Gamma1[int((i-1)/2)],i,i] for i in range(1,4*L,2)]
+        part2 = [[-1*self.Gamma1[int((i-1)/2),int((j-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
         #augmented
-        part3 = [[-1*self.Gamma1[int((i)/2)],i,i] for i in range(0,4*L,2)]
+        part3 = [[-1*self.Gamma1[int((i)/2),int((j)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
 
         static=[
             ["--",part1],
             ["+-",part2],
             ["+-",part3],  
             ]
+        
         dynamic=[]
+        h1=hamiltonian([["--",part1]],dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
+        #print(h1.basis)
+        #print('h1')
+        #print(h1)
+        h2=hamiltonian([["+-",part2]],dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
+        #print('h2')
+        #print(h2)
+        h3=hamiltonian([["+-",part3]],dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
+        #print('h3')
+        #print(h3)
         return hamiltonian(static,dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
     
     #add particels to the system
     def define_Dissipators2(self):
         L = self.L
         #mixed
-        part1 = [[-2j*self.Gamma2[int(i/2)],(i+1),(i)] for i in range(0,4*L,2)]
+        part1 = [[-2j*self.Gamma2[int(i/2),int((j-1)/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
         #fock
-        part2 = [[-1*self.Gamma2[int((i-1)/2)],i,i] for i in range(1,4*L,2)]
+        part2 = [[-1*self.Gamma2[int((i-1)/2),int((j-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
         #augmented
-        part3 = [[-1*self.Gamma2[int((i)/2)],i,i] for i in range(0,4*L,2)]
+        part3 = [[-1*self.Gamma2[int((i)/2),int((j)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
         
         static=[
             ["++",part1],
@@ -267,22 +300,59 @@ class createLindblad:
     
         return hamiltonian(static,dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
     
+    def define_Dissipators(self):
+        L = self.L
+        #mixed
+        part1_empty = [[-2j*self.Gamma1[int(i/2),int((j-1)/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
+        #part1_empty = [[-2j*self.Gamma1[int((j-1)/2),int(i/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
+        #Fock
+        part2_empty = [[-1*self.Gamma1[int((i-1)/2),int((j-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
+        #part2_empty = [[-1*self.Gamma1[int((j-1)/2),int((i-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
+        #augmented
+        part3_empty = [[-1*self.Gamma1[int((i)/2),int((j)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
+        #part3_empty = [[-1*self.Gamma1[int((j)/2),int((i)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
+
+        #mixed
+        #part1_full = [[-2j*self.Gamma2[int(i/2),int((j-1)/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
+        part1_full = [[-2j*self.Gamma2[int((j-1)/2),int(i/2)],(j),(i)] for i in range(0,4*L,2) for j in range(1,4*L,2)]
+        #fock
+        #part2_full = [[-1*self.Gamma2[int((i-1)/2),int((j-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
+        part2_full = [[-1*self.Gamma2[int((j-1)/2),int((i-1)/2)],i,j] for i in range(1,4*L,2) for j in range(1,4*L,2)]
+        #augmented
+        #part3_full = [[-1*self.Gamma2[int((i)/2),int((j)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
+        part3_full = [[-1*self.Gamma2[int((j)/2),int((i)/2)],j,i] for i in range(0,4*L,2) for j in range(0,4*L,2)]
+        
+        static=[
+            ["--",part1_empty],
+            ["+-",part2_empty],
+            ["+-",part3_empty],  
+            ["++",part1_full],
+            ["-+",part2_full],
+            ["-+",part3_full],
+            ]
+
+        dynamic=[]
+        return hamiltonian(static,dynamic,dtype=np.complex128,basis=self.basis,check_herm=False,check_pcon=False,check_symm=False)
+    
     def get_staticLindblad(self):
 
         H_super=self.defineH_super(self)
-        D1=self.define_Dissipators1(self)
-        D2=self.define_Dissipators2(self)
-    
+        #D1=self.define_Dissipators1(self)
+        #D2=self.define_Dissipators2(self)
+        D=self.define_Dissipators(self)
         #return -1j*H_super+D1+D2
-        return H_super+1j*(D1+D2)  # multiply by -i due to .evolve structure
+        #return H_super+1j*(D1+D2)  # multiply by -i due to .evolve structure
+        return H_super+1j*(D)
     
     def get_dynamicLindblad(self):
         H_super=self.defineH_super()
-        D1=self.define_Dissipators1()
-        D2=self.define_Dissipators2()
+        #D1=self.define_Dissipators1()
+        #D2=self.define_Dissipators2()
+        D=self.define_Dissipators()
     
         #return -1j*H_super+D1+D2
-        return H_super+1j*(D1+D2)
+        #return H_super+1j*(D1+D2)
+        return H_super+1j*(D)
     
     def exact_Diagonalization(self):
         L_static_csr=self.operator.as_sparse_format()

test_sites.py
@@ -38,13 +38,15 @@ parameters5 = {"length": 5,
 }
 
 parameters3 = {"length": 3,
-              "epsilon": [1,0,-1],
-              "hopping": 0.3,
+              "epsilon": [-4,0,4],
+              "hopping": [3,3,0],
               "interaction":0,
               "drive": 0,
               "frequency":0,
-              "coupling_empty":np.array([[0.9,0,0.0],[0,0,0],[0.0,0,0.6]]),
-              "coupling_full":np.array([[0.6,0,0.2],[0,0,0],[0.2,0,0.9]]),
+              #"coupling_empty":np.array([[2,0,0.2],[0,0,0],[0.2,0,0.6]])*2,
+              #"coupling_full":np.array([[0.6,0,0.1],[0,0,0],[0.1,0,2]])*2,
+              "coupling_empty":np.array([[0.7,0,0.4+0.8j],[0,0,0],[0.4-0.8j,0,5]]),
+              "coupling_full":np.array([[5,0,0.4+0.8j],[0,0,0],[0.4-0.8j,0,0.7]]),
               "spin_symmetric":False,
 }
 
@@ -131,7 +133,7 @@ GF0=calculateGreensFunction(parameters3,[[0,0]],'up')
 
 #print((GF0_sym.plus_lV.T.conjugate()@a))
 #n=GF0.plot_n(0,500)
-GF0._GreaterLesserSites([[0,0]],dt=0.05,eps=1e-8,max_iter=100,
+GF0._GreaterLesserSites([[0,0]],dt=0.05,eps=1e-8,max_iter=1000,
                    av_periods=4,tf=1.5e1,t_step=1.5e1,av_Tau=5,writeFile=True,
                     dirName='class_structure/results')
 print('total time: ',time.time()-t_start)